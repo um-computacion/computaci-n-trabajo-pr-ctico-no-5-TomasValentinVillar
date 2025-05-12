f9e25d632d243a275d03060832a61ba3c98fd492
Tomas Villar <t.villar@alumno.um.edu.ar>
2025-05-12 20:57:18 -0300

    Etapa 2: Agregado comportamiento de pensamiento  persona (quité el metodo __str__ por que al final no me pareció necesario)

--- src/persona.py
+++ src/persona.py
@@ -6,11 +6,16 @@ class Persona:
         self.pensamientos = 0
         self.ultima_idea = "<no penso en nada>"
 
-    #Implementé correctamente el método __str__ para satisfacer la 
-    #prueba test_repr_persona que está verificando la salida de str(persona).
-
-    def __str__(self):
-        return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
 
     def __repr__(self):
         return f"Persona: DNI: {self.dni} Nombre: {self.nombre} Apellido: {self.apellido} Ultima Idea: {self.ultima_idea}"
+    #Mejoré el método pensar para que revise que el pesamiento se texto y que no sea una cadena vacía
+    def pensar(self, idea):
+        if not isinstance(idea, str):
+            raise TypeError("La idea debe ser una cadena de texto")
+        
+        if not idea.strip():
+            raise ValueError("La idea no puede estar vacía")
+        self.pensamientos += 1
+        self.ultima_idea = idea
+    
